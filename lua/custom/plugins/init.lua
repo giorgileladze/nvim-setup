f9f73fb6fe5e19ebedc653c262285d1806384c8e
giorgileladze <giorgileladze78@gmail.com>
2024-11-03 18:13:31 +0400

    add harpoon

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,17 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'ThePrimeagen/harpoon',
+    config = function()
+      vim.keymap.set('n', '<leader>e', function()
+        require('harpoon.ui').toggle_quick_menu()
+      end, { desc = 'Toggle harpoon menu' })
+
+      vim.keymap.set('n', '<leader>a', function()
+        require('harpoon.mark').add_file()
+      end, { desc = 'Add file to harpoon' })
+    end,
+  },
+}
